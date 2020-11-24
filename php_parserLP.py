3c8acdee7f1c753e7357914f14d9fc9221510722
Miguel Parra <miguelparra@Miguels-MacBook-Pro.local>
2020-11-23 23:16:40 -0500

    Adicion de funciones para analizar expresiones y funciones extras

--- php_parserLP.py
+++ php_parserLP.py
@@ -53,6 +53,83 @@ def p_ambito(p):
                | CONST
     '''
 
+# En esta seccion se compara los valores usando los operadores
+def p_comparacion(p):
+    '''comparacion : valor operadorC expresion PTOCO
+                | valor operadorM expresion PTOCO
+                | valor operadorL expresion PTOCO
+    '''
+
+#Tipo de expresiones
+def p_expresion(p):
+    '''expresion : valor
+                 | expresion_aritmetica
+                 | expresion_logica
+                 | expresion_comparativa
+    '''
+
+#Formas de mostrar una expresion
+def p_expresion_aritmetica(p):
+    '''expresion_aritmetica : valor operadorM expresion
+    '''
+
+def p_expresion_logica(p):
+    '''expresion_logica : valor operadorL expresion
+                        | booleano
+    '''
+
+def p_expresion_comparativa(p):
+    '''expresion_comparativa : valor operadorC expresion
+    '''
+
+def p_condicion(p):
+    '''condicion : expresion_logica
+                | expresion_comparativa
+    '''
+#Operadores matematicos
+def p_operadorM(p):
+    '''operadorM : MAS
+                   | RESTA
+                   | PROD
+                   | DIV
+                   | MOD
+                   | POTENCIA
+    '''
+#Operadores matematicos
+def p_operadorC(p):
+    '''operadorC : MAYOR
+                   | MENOR
+                   | COMPARACION
+                   | IDENTICO
+                   | DIFERENTE
+                   | NOIDENTICO
+                   | MENOROIGUAL
+                   | MAYOROIGUAL
+                   | NAVEESPACIAL
+    '''
+#con esto se finaliza un php
+def p_terminar(p):
+    '''terminar : BREAK PTOCO
+    '''
+
+#Operadores logicos
+def p_operadorL(p):
+    '''operadorL : AND
+                  | XOR
+                  | OR
+                  | NOT
+    '''
+
+#se crea el valor de una variable
+def p_valor(p):
+    '''valor : INTEGER
+             | booleano
+             | STRING
+             | FLOAT
+             | VARIABLE
+             | BOOLEAN
+    '''
+
 def p_error(p):
     print("Syntax error in input!")
 
